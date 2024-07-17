d11178005683e21e46d3fb66e58e5aa1782d74f7
nbcormier <nicklaus.cormier@gmail.com>
2024-07-16 23:54:48 -0400

    mutiple accounts + first tee times

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,37 @@
-describe('template spec', () => {
-  /* ==== Test Created with Cypress Studio ==== */
-  it('bookTeeTime', function() {
-    /* ==== Generated with Cypress Studio ==== */
+describe('Natty Boyz Saturday Tee Times', () => {
+  it('Should book the 1st and 2nd tee times', function() {
     cy.visit('https://apps.invitedclubs.com/portal/pls/portal/!CCTTWEB.controller?ID=84c3c5f5fb53af90045241ec7cee8364');
     cy.get('#home_club').click();
-    cy.get('#Tab-20240717 > .cc-day').click();
-    cy.get('tr.cc-tee-time-row:last-child > td > span > span').click();
+    cy.get('div.cc-tab.cc-selectable:last-child').click();
+    cy.get('tr.cc-tee-time-row:nth-child(1) td.cc-multigrab-action.cc-selectable > span').click();
+    cy.get('.cc-ptype').each((playerSelect, index) => {
+      if (index !== 0)
+        cy.wrap(playerSelect).select('LOV_PT_TBD');
+    });
     cy.get('#save_button').click();
-    /* ==== End Cypress Studio ==== */
   });
-})
+
+  it('Should book the 3rd and 4th tee times', function() {
+    cy.visit('https://apps.invitedclubs.com/portal/pls/portal/!CCTTWEB.controller?ID=b6438ad2b25c7677493424362be7c57e');
+    cy.get('#home_club').click();
+    cy.get('div.cc-tab.cc-selectable:last-child').click();
+    cy.get('tr.cc-tee-time-row:nth-child(3) td.cc-multigrab-action.cc-selectable > span').click();
+    cy.get('.cc-ptype').each((playerSelect, index) => {
+      if (index !== 0)
+        cy.wrap(playerSelect).select('LOV_PT_TBD');
+    });
+    cy.get('#save_button').click();
+  });
+
+  it('Should book the 5th tee time', function() {
+    cy.visit('https://apps.invitedclubs.com/portal/pls/portal/!CCTTWEB.controller?ID=8124aa9e7003645953dea353eaea32c8');
+    cy.get('#home_club').click();
+    cy.get('div.cc-tab.cc-selectable:last-child').click();
+    cy.get('tr.cc-tee-time-row:nth-child(5) > td > span > span').click();
+    cy.get('.cc-ptype').each((playerSelect, index) => {
+      if (index !== 0)
+        cy.wrap(playerSelect).select('LOV_PT_TBD');
+    });
+    cy.get('#save_button').click();
+  });
+});
