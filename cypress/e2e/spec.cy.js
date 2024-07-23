9dd8a4fa0a18eb7a792fe65198bc8e7923cd2f5b
Nicklaus Cormier <ncormier@kyqrw5vvfw.manh.com>
2024-07-23 16:10:12 -0400

    refactor queries, setup for another sequental test run

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,35 +3,35 @@ describe('Natty Boyz Saturday Tee Times', () => {
     cy.visit('https://apps.invitedclubs.com/portal/pls/portal/!CCTTWEB.controller?ID=84c3c5f5fb53af90045241ec7cee8364');
     cy.get('#home_club').click();
     cy.get('div.cc-tab.cc-selectable:last-child').click();
-    cy.get('tr.cc-tee-time-row:nth-child(1) td.cc-multigrab-action.cc-selectable > span').click();
+    cy.get('.cc-blank-td-row').parent().siblings().eq(0).find('.cc-multigrab-action.cc-selectable').click();
     cy.get('.cc-ptype').each((playerSelect, index) => {
       if (index !== 0)
         cy.wrap(playerSelect).select('LOV_PT_TBD');
     });
-    cy.get('#save_button').click();
+    // cy.get('#save_button').click();
   });
 
   it('Should book the 3rd and 4th tee times', function() {
     cy.visit('https://apps.invitedclubs.com/portal/pls/portal/!CCTTWEB.controller?ID=b6438ad2b25c7677493424362be7c57e');
     cy.get('#home_club').click();
     cy.get('div.cc-tab.cc-selectable:last-child').click();
-    cy.get('tr.cc-tee-time-row:nth-child(3) td.cc-multigrab-action.cc-selectable > span').click();
+    cy.get('.cc-blank-td-row').parent().siblings().eq(2).find('.cc-multigrab-action.cc-selectable').click();
     cy.get('.cc-ptype').each((playerSelect, index) => {
       if (index !== 0)
         cy.wrap(playerSelect).select('LOV_PT_TBD');
     });
-    cy.get('#save_button').click();
+    // cy.get('#save_button').click();
   });
 
   it('Should book the 5th tee time', function() {
     cy.visit('https://apps.invitedclubs.com/portal/pls/portal/!CCTTWEB.controller?ID=8124aa9e7003645953dea353eaea32c8');
     cy.get('#home_club').click();
     cy.get('div.cc-tab.cc-selectable:last-child').click();
-    cy.get('tr.cc-tee-time-row:nth-child(5) > td > span > span').click();
+    cy.get('.cc-blank-td-row').parent().siblings().eq(4).find('.cc-reserve-button cc-selectable').click();
     cy.get('.cc-ptype').each((playerSelect, index) => {
       if (index !== 0)
         cy.wrap(playerSelect).select('LOV_PT_TBD');
     });
-    cy.get('#save_button').click();
+    // cy.get('#save_button').click();
   });
 });
