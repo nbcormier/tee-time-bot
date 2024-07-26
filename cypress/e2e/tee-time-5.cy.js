cfb3459c9be0ff63976c470bf35d50697caeb753
Nicklaus Cormier <ncormier@kyqrw5vvfw.manh.com>
2024-07-26 15:02:37 -0400

    fix 5th tee time selector, setup for trial run

--- cypress/e2e/tee-time-5.cy.js
+++ cypress/e2e/tee-time-5.cy.js
@@ -3,7 +3,7 @@ describe('Natty Boyz Saturday Tee Times - 5th', () => {
     cy.visit('https://apps.invitedclubs.com/portal/pls/portal/!CCTTWEB.controller?ID=8124aa9e7003645953dea353eaea32c8');
     cy.get('#home_club').click();
     cy.get('div.cc-tab.cc-selectable:last-child').click();
-    cy.get('.cc-blank-td-row').parent().siblings().eq(19).find('.cc-reserve-button cc-selectable').click();
+    cy.get('.cc-blank-td-row').parent().siblings().eq(19).find('.cc-reserve-button.cc-selectable').click();
     cy.get('.cc-ptype').each((playerSelect, index) => {
       if (index !== 0)
         cy.wrap(playerSelect).select('LOV_PT_TBD');
